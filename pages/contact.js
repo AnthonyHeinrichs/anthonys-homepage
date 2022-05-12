1ee68ccdf8b9c5c391eca8e621557c7070962edf
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-05-12 13:31:21 +0200

    Contact form with validation & success message

--- pages/contact.js
+++ pages/contact.js
@@ -1,52 +1,95 @@
-import { Container, Box, Heading, Input, Button, useColorModeValue } from '@chakra-ui/react'
+import { Container, Box, Heading, Input, Button, useColorModeValue, useToast } from '@chakra-ui/react'
 import { useRef } from 'react'
+import { useForm } from 'react-hook-form'
 import emailjs from 'emailjs-com'
 import Section from '../components/section'
 import {
   FormControl,
   FormLabel,
   FormErrorMessage,
-  FormHelperText,
 } from '@chakra-ui/react'
 
 const Contact = () => {
   const form = useRef();
+  const toast = useToast()
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm()
 
+  function onSubmit(values) {
     emailjs.sendForm('service_bdo7xz2', 'template_80ivt87', form.current, 'kwKSPUi0QO0oI8i6K')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        toast({
+          title: 'Message sent.',
+          description: "Thanks for sending me a message!",
+          status: 'success',
+          duration: 9000,
+          isClosable: true,
+        })
+        resolve()
+      }, 3000)
+    })
+  }
 
   return (
-<Container>
-  <Box display={{ md: 'flex' }} mt={100}>
-    <Box flexGrow={1}>
-      <Heading as='h2' variant='page-title'>
-        Send me a message
-      </Heading>
-    </Box>
-  </Box>
-  <Section delay={0.1}>
-    <form ref={form} onSubmit={sendEmail}>
-      <FormControl>
-        <FormLabel mt={8}>Name</FormLabel>
-        <Input name='user_name' type='text'/>
-        <FormLabel mt={4}>Email address</FormLabel>
-        <Input name='user_email' type='email' />
-        <FormHelperText>I will never share your email with anyone else.</FormHelperText>
-        <FormLabel mt={4} size='lg'>Message</FormLabel>
-        <Input name='message' type='message' />
-        <Button onClick={sendEmail} colorScheme={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} variant='outline' mt={4} type='sumbit'>Submit</Button>
-      </FormControl>
-    </form>
-  </Section>
-</Container>
+    <Container>
+      <Box display={{ md: 'flex' }} mt={100}>
+      <Box flexGrow={1}>
+        <Heading as='h2' variant='page-title'>
+          Send me a message
+        </Heading>
+      </Box>
+      </Box>
+      <Section delay={0.1}>
+        <form ref={form} onSubmit={handleSubmit(onSubmit)}>
+          <FormControl isInvalid={errors.name}>
+            <Input
+              id='name'
+              placeholder='Name'
+              name='user_name'
+              borderColor={{ color: 'inherit' }}
+              _placeholder={{ color: 'inherit' }}
+              {...register('name', {
+                required: 'This is required',
+              })} mt={4}
+            />
+            <Input
+              id='email'
+              placeholder='Email'
+              name='user_email'
+              borderColor={{ color: 'inherit' }}
+              _placeholder={{ color: 'inherit' }}
+              {...register('email', {
+                required: 'This is required',
+                minLength: { value: 4, message: 'Minimum length should be 4' },
+              })} mt={4}
+            />
+            <Input
+              id='message'
+              placeholder='Message'
+              name='message'
+              borderColor={{ color: 'inherit' }}
+               _placeholder={{ color: 'inherit' }}
+              {...register('message', {
+                required: 'This is required',
+                minLength: { value: 4, message: 'Minimum length should be 4' },
+              })} mt={4}
+            />
+            <FormErrorMessage>
+              {errors.name && errors.name.message}
+            </FormErrorMessage>
+          </FormControl>
+          <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit' variant='solid'>
+            Submit
+          </Button>
+        </form>
+      </Section>
+    </Container>
   )
 };
 
--- pages/cv.js
+++ pages/cv.js
@@ -1,6 +1,7 @@
 import { Center, Button, useColorModeValue, Container, SimpleGrid } from '@chakra-ui/react'
 import { saveAs } from 'file-saver'
 import CustomModal from '../components/modal'
+import Section from '../components/section'
 
 const Cv = () => {
   const clickHandler = () => {
@@ -12,18 +13,20 @@ const Cv = () => {
 
   return (
     <Container>
-      <Center>
-        <SimpleGrid columns={[1]}>
-          <Center mt={8}>
-            <Button colorScheme={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} variant='outline' onClick={clickHandler}>
-              Download my Resume
-            </Button>
-          </Center>
-          <Center mt={8}>
-            <CustomModal />
-          </Center>
-        </SimpleGrid>
-      </Center>
+      <Section delay={0.1}>
+        <Center>
+          <SimpleGrid columns={[1]}>
+            <Center mt={8}>
+              <Button colorScheme={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} variant='outline' onClick={clickHandler}>
+                Download my Resume
+              </Button>
+            </Center>
+            <Center mt={8}>
+              <CustomModal />
+            </Center>
+          </SimpleGrid>
+        </Center>
+      </Section>
     </Container>
   )
 }
