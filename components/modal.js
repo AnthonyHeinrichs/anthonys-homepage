9793227bf5a22415d88f265bec1ba0301fa89f97
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-05-12 14:08:56 +0200

    background for modal

--- components/modal.js
+++ components/modal.js
@@ -1,13 +1,15 @@
-import { Modal, ModalOverlay, ModalContent, useDisclosure, Image } from '@chakra-ui/react'
+import { Modal, ModalOverlay, ModalContent, useDisclosure, Image, Box } from '@chakra-ui/react'
 
 const CustomModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
     <>
-      <Image onClick={onOpen} borderColor='whiteAlpha.800' borderStyle='solid' maxWidth='400px' src='/images/anthony-heinrichs-cv.png' alt='Profile Pciture' />
+      <Box boxShadow='dark-lg' p='6' rounded='md' bg='white'>
+        <Image onClick={onOpen} borderColor='whiteAlpha.800' borderStyle='solid' maxWidth='360px' src='/images/anthony-heinrichs-cv.png' alt='Profile Pciture' />
+      </Box>
 
-      <Modal onClose={onClose} size='full' isOpen={isOpen} isCentered>
+      <Modal onClose={onClose} isOpen={isOpen} isCentered>
         <ModalOverlay />
         <ModalContent>
           <Image src='/images/anthony-heinrichs-cv.png' alt='Profile Pciture' />
