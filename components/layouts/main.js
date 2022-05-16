fbb2086625690d8ec67b74b2f1f96241bb167d00
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-05-16 15:23:55 +0200

    added footer

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Navbar from '../navbar.js'
+import Footer from '../footer.js'
 import { Box, Container } from '@chakra-ui/react'
 
 const Main = ({ children, router}) => {
@@ -9,12 +10,11 @@ const Main = ({ children, router}) => {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>Anthony Heinrichs - Profile</title>
       </Head>
-      
       <Navbar path={router.asPath} />
-
       <Container maxW='container.md' pt={14}>
         {children}
       </Container>
+      <Footer />
     </Box>
   )
 }
