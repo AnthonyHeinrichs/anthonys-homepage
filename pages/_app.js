e90348fd50b0ac552c6332e57fda51db74c464d9
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-06-01 14:14:41 +0200

    Meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head';
 import Layout from '../components/layouts/main'
 import theme from '../lib/theme'
 import Fonts from '../components/fonts'
@@ -6,14 +7,20 @@ import { AnimatePresence } from 'framer-motion'
 
 const Website = ({ Component, pageProps, router }) => {
   return (
-    <ChakraProvider theme={theme}>
-      <Fonts />
-      <Layout router={router}>
-        <AnimatePresence exitBeforeEnter intial={true}>
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
-      </Layout>
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Anthony Heinrichs - Developer Profile</title>
+        <link rel='icon' href='/images/profilelogo.svg' />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Fonts />
+        <Layout router={router}>
+          <AnimatePresence exitBeforeEnter intial={true}>
+            <Component {...pageProps} key={router.route} />
+          </AnimatePresence>
+        </Layout>
+      </ChakraProvider>
+    </>
   )
 }
 
--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,4 @@
-import { Container, Box, Heading, AspectRatio, Link, Image } from '@chakra-ui/react'
+import { Container, Box, Heading, AspectRatio, Link, Center, Image } from '@chakra-ui/react'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import Layout from '../components/layouts/article'
@@ -16,11 +16,41 @@ const Projects = () => {
           </Box>
         </Box>
         <Section delay={0.1}>
-          <Link href='https://www.dish-list.com/' isExternal>
-            <Heading as='h3' variant='section-title' mt={6}>
-              Dish List
-            </Heading>
-          </Link>
+        <Link href='https://help-local-germany.vercel.app/' isExternal>
+          <Heading as='h3' variant='section-title' mt={8}>
+            Help Local - In Progress
+          </Heading>
+        </Link>
+          <Box mb={2}>
+            <em> Next.js - Node.js - React - SASS </em>
+          </Box>
+          <Paragraph>
+            Help Local is a collaborative mobile application. The purpose of help local is to connect people in need
+            with someone who has something to give. With help local, you can open requests or add offers for different
+            needs.
+          </Paragraph>
+          <Center>
+            <Image src='/images/help-local.png' alt='Help Local' mt={6} mb={2}/>
+          </Center>
+        </Section>
+        <Section delay={0.1}>
+        <Link href='https://my-travel-journal-fawn.vercel.app/' isExternal>
+          <Heading as='h3' variant='section-title' mt={8}>
+            Travel Journal
+          </Heading>
+        </Link>
+          <Box mb={2}>
+            <em> React - JSX - CSS </em>
+          </Box>
+          <Paragraph>
+            Basic travel journal I built one morning to document all the different places I have traveled to while living in Europe.
+          </Paragraph>
+          <Image src='/images/travel-journal.jpg' alt='My Travel Journal' mt={6} mb={2}/>
+        </Section>
+        <Section delay={0.1}>
+          <Heading as='h3' variant='section-title' mt={6}>
+            Dish List
+          </Heading>
           <Box mb={2}>
             <em> Ruby - Rails - HTML - Sass - PostgreSQL - Heroku </em>
           </Box>
@@ -36,36 +66,6 @@ const Projects = () => {
               allowFullScreen
             />
           </AspectRatio>
-        </Section>
-        <Section delay={0.1}>
-        <Link href='https://help-local-germany.vercel.app/' isExternal>
-          <Heading as='h3' variant='section-title' mt={8}>
-            Help Local - In Progress
-          </Heading>
-        </Link>
-          <Box mb={2}>
-            <em> Next.js - Node.js - React - SASS </em>
-          </Box>
-          <Paragraph>
-            Help Local is a collaborative mobile application. The purpose of help local is to connect people in need
-            with someone who has something to give. With help local, you can open requests or add offers for different
-            needs.
-          </Paragraph>
-          <Image src='/images/help-local.png' alt='Help Local' mt={6} mb={2}/>
-        </Section>
-        <Section delay={0.1}>
-        <Link href='https://my-travel-journal-fawn.vercel.app/' isExternal>
-          <Heading as='h3' variant='section-title' mt={8}>
-            Travel Journal
-          </Heading>
-        </Link>
-          <Box mb={2}>
-            <em> React - JSX - CSS </em>
-          </Box>
-          <Paragraph>
-            Basic travel journal I built one morning to document all the different places I have traveled to while living in Europe.
-          </Paragraph>
-          <Image src='/images/travel-journal.jpg' alt='My Travel Journal' mt={6} mb={2}/>
         </Section>
       </Container>
     </Layout>
