78445dc3f16beac2dc7616178a88ea273f066a4a
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-06-02 12:09:13 +0200

    Updated head

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Navbar from '../navbar.js'
 import Footer from '../footer.js'
 import { Box, Container } from '@chakra-ui/react'
@@ -9,7 +10,9 @@ const Main = ({ children, router}) => {
       <Box as='main' pb={6} minH='95vh'>
         <Head>
           <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <title>Anthony Heinrichs - Profile</title>
+          <title>Anthony Heinrichs - Developer Profile</title>
+          <link rel='icon' href='/images/profilelogo.svg' />
+          <Image src='/images/developer-site.jpg' alt='Anthony Heinrichs' layout='fill' />
         </Head>
         <Navbar path={router.asPath} />
         <Container maxW='container.md' pt={14}>
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import Head from 'next/head'
-import Image from 'next/image'
 import Layout from '../components/layouts/main'
 import theme from '../lib/theme'
 import Fonts from '../components/fonts'
@@ -8,21 +6,14 @@ import { AnimatePresence } from 'framer-motion'
 
 const Website = ({ Component, pageProps, router }) => {
   return (
-    <>
-      <Head>
-        <title>Anthony Heinrichs - Developer Profile</title>
-        <link rel='icon' href='/images/profilelogo.svg' />
-        <Image alt='Anthony Heinrichs' src='/images/developer-site.jpg' layout='fill' />
-      </Head>
-      <ChakraProvider theme={theme}>
-        <Fonts />
-        <Layout router={router}>
-          <AnimatePresence exitBeforeEnter intial={true}>
-            <Component {...pageProps} key={router.route} />
-          </AnimatePresence>
-        </Layout>
-      </ChakraProvider>
-    </>
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <Layout router={router}>
+        <AnimatePresence exitBeforeEnter intial={true}>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </ChakraProvider>
   )
 }
 
